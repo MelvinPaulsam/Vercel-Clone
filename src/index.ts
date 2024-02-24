44abd4b551b2b216b3d2937c0c5157fa802f07ed
Melvin Paulsam <003melvin@gmail.com>
2024-02-24 22:37:21 +0530

    Added caching

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ import { getAllFiles } from "./file";
 import simpleGit from "simple-git";
 import { uploadFile } from "./aws";
 import path from "path";
+import { createClient } from "redis";
+const publisher = createClient();
+publisher.connect();
+
+const subscriber = createClient();
+subscriber.connect();
 const app=express();
 app.use(cors());
 app.use(express.json())
@@ -18,8 +24,17 @@ app.post("/deploy",async (req,res)=>{
 files.forEach(async file => {
     await uploadFile(file.slice(__dirname.length + 1), file);
 })
+publisher.lPush("build-queue", id);
+publisher.hSet("status", id, "uploaded");
     res.json({
         id: id
+    })
+});
+app.get("/status", async (req, res) => {
+    const id = req.query.id;
+    const response = await subscriber.hGet("status", id as string);
+    res.json({
+        status: response
     })
 })
 app.listen(3000);
